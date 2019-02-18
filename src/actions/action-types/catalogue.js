f98f0033bdfeda40c325f9de5c328a9ff5497087
prod1992 <levon1.grigoryan@gmail.com>
2019-02-18 19:32:50 +0400

    updates

---
+++ src/actions/action-types/catalogue.js
@@ -0,0 +1,5 @@
+/* catalogue functional buttons */
+
+export const BULK_EDIT = "BULK_EDIT";
+export const COLLECT_SELECTED_IDS = "COLLECT_SELECTED_IDS";
+export const SELECT_ALL_PRODUCTS = "SELECT_ALL_PRODUCTS";
