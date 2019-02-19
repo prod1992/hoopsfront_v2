d8293eb662827ae56b0d657386b81257e9637b2f
ArturHarutunyan <artur.harutunyan1999@gmail.com>
2019-02-19 21:37:30 +0400

    Updates

---
+++ precache-manifest.c832594c2655d3449ea3086938561c66.js
@@ -0,0 +1,206 @@
+self.__precacheManifest = [
+  {
+    "revision": "c87c0819b7ce9ab1503429d424ffa18c",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraLight.c87c0819.eot"
+  },
+  {
+    "revision": "2d1797be94b4315c071d",
+    "url": "/hoopsfront-v2/static/css/main.93eaf281.chunk.css"
+  },
+  {
+    "revision": "36dd8628f7136cdc4d3c",
+    "url": "/hoopsfront-v2/static/js/runtime~main.36dd8628.js"
+  },
+  {
+    "revision": "0c5469c3c3a4d5d687dfa8b5183dd03e",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Bold.0c5469c3.svg"
+  },
+  {
+    "revision": "3d903ea3bfbb09b05077",
+    "url": "/hoopsfront-v2/static/js/2.3d903ea3.chunk.js"
+  },
+  {
+    "revision": "8cefd3244afaa816f3f56d78c0577114",
+    "url": "/hoopsfront-v2/static/media/Catamaran-SemiBold.8cefd324.woff2"
+  },
+  {
+    "revision": "e87990de40b041eb3a4bc0b822e6c2c7",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraLight.e87990de.woff2"
+  },
+  {
+    "revision": "c28ef0c18a322b426891527ff43beb9e",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Regular.c28ef0c1.woff2"
+  },
+  {
+    "revision": "b7e94363a4ccad2bfe297fcbf496ada4",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Thin.b7e94363.woff2"
+  },
+  {
+    "revision": "4147ce782d79c22e18dbdbc57ec9897f",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Black.4147ce78.woff2"
+  },
+  {
+    "revision": "2554b52e7d57b8fa512c153a640b7416",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Light.2554b52e.woff2"
+  },
+  {
+    "revision": "0fe9ce9eb518f2ceb3118a530d2259c7",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraBold.0fe9ce9e.woff2"
+  },
+  {
+    "revision": "567f11ef163fec997f3fa0ae0c0f3e48",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Medium.567f11ef.woff2"
+  },
+  {
+    "revision": "d192a4712365be4dc052998a9746030d",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Bold.d192a471.woff2"
+  },
+  {
+    "revision": "dcdaac6afc94c4c9cf19ce532de17325",
+    "url": "/hoopsfront-v2/static/media/Catamaran-SemiBold.dcdaac6a.woff"
+  },
+  {
+    "revision": "8ff619eb3418309435a0cb798e562f86",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraLight.8ff619eb.woff"
+  },
+  {
+    "revision": "f922eda3dbe9b8252fcb131b3922cf8c",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Regular.f922eda3.woff"
+  },
+  {
+    "revision": "c5f35113b4e0ecbdc91802f7663c68d2",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Thin.c5f35113.woff"
+  },
+  {
+    "revision": "eb3ceb339a828d19c18cf30c69fbce1c",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Black.eb3ceb33.woff"
+  },
+  {
+    "revision": "a461035c9bde0a8ca6ce7d45a7bde8fe",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Light.a461035c.woff"
+  },
+  {
+    "revision": "f225697abd5a56a3cb3d110eca5b42f4",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraBold.f225697a.woff"
+  },
+  {
+    "revision": "e0b2d535d20e3536dafc79e2908cf9e6",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Medium.e0b2d535.woff"
+  },
+  {
+    "revision": "fda9c9f50c20306dc1cddda4647b6fa1",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Bold.fda9c9f5.woff"
+  },
+  {
+    "revision": "8e7fbf945f549e42c75e79921ecea0bc",
+    "url": "/hoopsfront-v2/static/media/Catamaran-SemiBold.8e7fbf94.eot"
+  },
+  {
+    "revision": "cbe7abc136fcf867669cc79b631af7b4",
+    "url": "/hoopsfront-v2/static/media/Catamaran-SemiBold.cbe7abc1.ttf"
+  },
+  {
+    "revision": "2d1797be94b4315c071d",
+    "url": "/hoopsfront-v2/static/js/main.2d1797be.chunk.js"
+  },
+  {
+    "revision": "fb2b46546ef1c47c3c73768474fab7e2",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraLight.fb2b4654.ttf"
+  },
+  {
+    "revision": "a2eb4586213b83ee61402d03703bb305",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Regular.a2eb4586.ttf"
+  },
+  {
+    "revision": "5b3d976fb3544ea592de03f8a46067ba",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Regular.5b3d976f.eot"
+  },
+  {
+    "revision": "b82697231d4c79f8f0d10f05b5851a67",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Thin.b8269723.eot"
+  },
+  {
+    "revision": "6410249f3582d5ae2f864064ebc89c9e",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Thin.6410249f.ttf"
+  },
+  {
+    "revision": "8f8e6e055bf79b0fdcf9dac349c8b573",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Black.8f8e6e05.eot"
+  },
+  {
+    "revision": "405d951c943f334cdf7c6b0b663e9c4a",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Black.405d951c.ttf"
+  },
+  {
+    "revision": "102cd6e2cc25c9b712a443ccd1066e49",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Light.102cd6e2.eot"
+  },
+  {
+    "revision": "4ff2477a04ed45eae7901cd22df8e5e3",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Light.4ff2477a.ttf"
+  },
+  {
+    "revision": "a57b4e14b285d5bf077d38bd1b694601",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraBold.a57b4e14.eot"
+  },
+  {
+    "revision": "3efd021328f3213d216e783675d9e858",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraBold.3efd0213.ttf"
+  },
+  {
+    "revision": "ef441c2c3c4b11b6da9f20bb97b3266e",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Medium.ef441c2c.eot"
+  },
+  {
+    "revision": "c5cfae47125ed6618d7a7fa68cc01f69",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Medium.c5cfae47.ttf"
+  },
+  {
+    "revision": "d9e4e589960cb2bf826642cba5d212b7",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Bold.d9e4e589.eot"
+  },
+  {
+    "revision": "946c0fcb60539eac2a7fbbf0548fd392",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Bold.946c0fcb.ttf"
+  },
+  {
+    "revision": "9fdd9b3d565ff924a9bd538c73f40612",
+    "url": "/hoopsfront-v2/static/media/Catamaran-SemiBold.9fdd9b3d.svg"
+  },
+  {
+    "revision": "ad07cdd4688be27fb9a5da1cdda1252a",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraLight.ad07cdd4.svg"
+  },
+  {
+    "revision": "8b62fad851c81a387797b05e61f94a92",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Regular.8b62fad8.svg"
+  },
+  {
+    "revision": "0f5c1f4bfddfdf24561829d33c586e77",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Thin.0f5c1f4b.svg"
+  },
+  {
+    "revision": "a4eababaac6b3605f91d6e9fda848420",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Black.a4eababa.svg"
+  },
+  {
+    "revision": "d4c0e2a8abf8ca1525420c0b0382c6b0",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Light.d4c0e2a8.svg"
+  },
+  {
+    "revision": "61c629394d91854797aeec7ef7a2fcd2",
+    "url": "/hoopsfront-v2/static/media/Catamaran-ExtraBold.61c62939.svg"
+  },
+  {
+    "revision": "f41473d964b8036c12e7937d072e0d9d",
+    "url": "/hoopsfront-v2/static/media/Catamaran-Medium.f41473d9.svg"
+  },
+  {
+    "revision": "3d903ea3bfbb09b05077",
+    "url": "/hoopsfront-v2/static/css/2.7bc08086.chunk.css"
+  },
+  {
+    "revision": "285d63c0f5021b366b8afcacfc14f6f7",
+    "url": "/hoopsfront-v2/index.html"
+  }
+];
