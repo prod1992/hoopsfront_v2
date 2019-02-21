49feff649903a781365e89009c06fe6c00d87c7b
prod1992 <levon1.grigoryan@gmail.com>
2019-02-21 20:27:46 +0400

    Griid view

--- src/components/catalogue/index.js
+++ src/components/catalogue/index.js
@@ -18,12 +18,17 @@ import Chip from "@material-ui/core/Chip";
 const styles = theme => ({
   card: {
     maxWidth: "100%",
+
     borderRadius: 3,
     boxShadow: "rgba(0, 0, 0, 0) 0px 0px 8px 0px",
     transition: "0.2s ease box-shadow",
+
     "&:hover": {
       boxShadow: "rgba(0, 0, 0, 0.2) 0px 0px 8px 0px"
     }
+  },
+  cardContent: {
+    flex: 1
   },
   media: {
     height: 0,
@@ -34,6 +39,10 @@ const styles = theme => ({
   },
   productInfo: {
     fontSize: "0.9rem"
+  },
+  gridView: {
+    display: "flex",
+    flexDirection: "row"
   }
 });
 
@@ -100,53 +109,77 @@ class SimpleProduct extends Component {
 
     const GridItemView = props => {
       return (
-        <div className={classes.productInfo}>
-          <div className="custom-row flex-wrapper">
-            <div className="left-part">
-              <span className="product-name">{simpleProduct.name}</span>
-              <Chip label={simpleProduct.id} />
-            </div>
-            <div className="right-part custom-col-2">
-              <span className="price">
-                $<span>{simpleProduct.product_price || "0.00"}</span>
-              </span>
-            </div>
-          </div>
-          <div>
-            <div className="brand">
-              <span>
-                Brand: <strong>{simpleProduct.brand}</strong>
-              </span>
-            </div>
-            <div className="category">
-              <span className="grey-text">|</span>
-              <span>
-                Category: <strong>{simpleProduct.category}</strong>
-              </span>
-            </div>
-            <div className="vendor">
-              <span className="grey-text">|</span>
-              <span>
-                Vendor: <strong>Example</strong>
-              </span>
-            </div>
-            <span className="price-text">BUY PRICE</span>
-          </div>
-        </div>
+        <Grid container className={classes.productInfo}>
+          <Grid item container xs={12}>
+            <Grid item xs>
+              <Grid item>
+                <span className="product-name">{simpleProduct.name}</span>
+                <Chip label={simpleProduct.id} />
+              </Grid>
+              <Grid item container>
+                <Grid item className="brand">
+                  <span>
+                    Brand: <strong>{simpleProduct.brand}</strong>
+                  </span>
+                </Grid>
+                <Grid item className="category">
+                  <span className="grey-text">|</span>
+                  <span>
+                    Category: <strong>{simpleProduct.category}</strong>
+                  </span>
+                </Grid>
+                <Grid item className="vendor">
+                  <span>
+                    Vendor: <strong>Example</strong>
+                  </span>
+                </Grid>
+              </Grid>
+            </Grid>
+            <Grid item xs={2}>
+              <div className="price">
+                <div>
+                  $<span>{simpleProduct.product_price || "0.00"}</span>
+                </div>
+                <div>
+                  <span className="price-text">BUY PRICE</span>
+                </div>
+              </div>
+            </Grid>
+          </Grid>
+        </Grid>
       );
     };
     return (
       <Grid
         item
-        xs={item_view_class_name.xs}
-        md={item_view_class_name.md}
-        lg={item_view_class_name.lg}
-        xl={item_view_class_name.xl}
-        className={
-          viewType !== PRODUCT_VIEW_TYPE["col_view"] ? grid_view_class_name : ""
+        xs={
+          viewType !== PRODUCT_VIEW_TYPE["col_view"]
+            ? 12
+            : item_view_class_name.xs
+        }
+        md={
+          viewType !== PRODUCT_VIEW_TYPE["col_view"]
+            ? 12
+            : item_view_class_name.md
+        }
+        lg={
+          viewType !== PRODUCT_VIEW_TYPE["col_view"]
+            ? 12
+            : item_view_class_name.lg
+        }
+        xl={
+          viewType !== PRODUCT_VIEW_TYPE["col_view"]
+            ? 12
+            : item_view_class_name.xl
         }
       >
-        <Card className={classes.card}>
+        <Card
+          className={
+            classes.card +
+            " " +
+            (viewType !== PRODUCT_VIEW_TYPE["col_view"] ? classes.gridView : "")
+          }
+        >
           {bulkEdit && (
             <div className="catalogue-product-check">
               <div className="checkbox-ctrl">
@@ -169,8 +202,14 @@ class SimpleProduct extends Component {
             </div>
           )}
           <Link
+            style={{
+              display: "block",
+              flex: "1 0 66px",
+              maxWidth:
+                viewType !== PRODUCT_VIEW_TYPE["col_view"] ? 66 : "100%",
+              padding: viewType !== PRODUCT_VIEW_TYPE["col_view"] ? 20 : 0
+            }}
             to={`/product/${simpleProduct.id}`}
-            className="product-image-frame"
           >
             <CardMedia
               className={classes.media}
@@ -178,7 +217,7 @@ class SimpleProduct extends Component {
               title={simpleProduct.title}
             />
           </Link>
-          <CardContent>
+          <CardContent className={classes.cardContent}>
             {viewType === PRODUCT_VIEW_TYPE["col_view"] ? (
               <ColumnItemView simpleProduct={simpleProduct} />
             ) : (
